d8f98050ee4975c2010f061cb7b2bb944b504103
LydiahLaw <Lygashiku@gmail.com>
2026-02-01 17:21:57 +0300

    add all the changes made

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,45 +1,38 @@
 <?php
 
-use App\Task;
+namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\Models\Task; // make sure your Task model is in App\Models
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    use DatabaseTransactions;
-
+    use RefreshDatabase;
 
     public function test_tasks_are_displayed_on_the_dashboard()
     {
-        factory(Task::class)->create(['name' => 'Task 1']);
-        factory(Task::class)->create(['name' => 'Task 2']);
-        factory(Task::class)->create(['name' => 'Task 3']);
+        Task::factory()->create(['name' => 'Task 1']);
+        Task::factory()->create(['name' => 'Task 2']);
+        Task::factory()->create(['name' => 'Task 3']);
 
-        $this->visit('/')
-             ->see('Task 1')
-             ->see('Task 2')
-             ->see('Task 3');
+        $response = $this->get('/');
+        $response->assertSee('Task 1')
+                 ->assertSee('Task 2')
+                 ->assertSee('Task 3');
     }
-
 
     public function test_tasks_can_be_created()
     {
-        $this->visit('/')->dontSee('Task 1');
+        $response = $this->post('/tasks', ['name' => 'Task 1']);
+        $response->assertRedirect('/'); // assuming you redirect after creation
 
-        $this->visit('/')
-            ->type('Task 1', 'name')
-            ->press('Add Task')
-            ->see('Task 1');
+        $this->assertDatabaseHas('tasks', ['name' => 'Task 1']);
     }
-
 
     public function test_long_tasks_cant_be_created()
     {
-        $this->visit('/')
-            ->type(str_random(300), 'name')
-            ->press('Add Task')
-            ->see('Whoops!');
+        $response = $this->post('/tasks', ['name' => str_repeat('a', 300)]);
+        $response->assertSessionHasErrors(); // expects validation errors
     }
 }
